14ee37f2505baafa19deb107b82ef5255ade2f4d
Thomas Ellwood <taellwood@gmail.com>
2025-01-12 15:31:35 +0000

    Built routes for beers, breweries and styles

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask
+import json
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+# Load data from JSON file
+with open("data.json") as f:
+    data = json.load(f)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/breweries", methods=["GET"])
+def get_breweries():
+    return jsonify(data["breweries"])
+
+@app.route("/beers", methods=["GET"])
+def get_beers():
+    return jsonify(data["beers"])
+
+@app.route("/styles", methods=["GET"])
+def get_styles():
+    return jsonify(data["styles"])
 
 if __name__ == "__main__":
     app.run(debug=True)
